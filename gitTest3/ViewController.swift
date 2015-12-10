526036b16458d4df53b881a734fa7b7a28d29461
Matt James <mmemmo@gmail.com>
2015-12-09 20:10:19 -0500

    Change number two??

--- gitTest3/ViewController.swift
+++ gitTest3/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Hey, Ho");
         print ("Change two");
+        print("fire");
     }
 
     override func didReceiveMemoryWarning() {
