e04b11975b79446f0e3b45639434d93160d3a7c0
Matt James <mmemmo@gmail.com>
2015-12-09 20:03:17 -0500

    Initial Commit

---
+++ gitTest3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gitTest3
+//
+//  Created by Matt Memmo on 12/9/15.
+//  Copyright © 2015 EA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hey, Ho");
+        print ("Change two");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
